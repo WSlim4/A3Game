57b0a19575e2cbc1cc54b236b2eb949eed77d0d8
SamuLSampy <samuelbsamp@gmail.com>
2025-06-09 23:28:20 -0300

    Adicionado obstaculos

--- src/Game.java
+++ src/Game.java
@@ -5,11 +5,7 @@ import Update.Update;
 import Render.Render;
 import ProcessInput.ProcessInput;
 import Player.Player;
-
-
-
 import javax.swing.*;
-import java.awt.event.KeyEvent;
 
 public class Game {
     private Update UpdateState;
@@ -29,10 +25,13 @@ public class Game {
         this.player = new Player(pontos);
         this.dust = new Dust(player);
         this.UserInput = new ProcessInput(player);
+        this.UpdateState = new Update(player, obstaculo);
+
+        this.pontos.setObstaculos(UpdateState.getObstaculos());
 
 
         // Cria painel de jogo e passa os objetos
-        this.gamePanel = new GamePanel(player, dust, pontos, obstaculo);
+        this.gamePanel = new GamePanel(player, dust, pontos, obstaculo, UpdateState);
         gamePanel.addKeyListener(this.UserInput);
         gamePanel.setFocusable(true);
         gamePanel.requestFocusInWindow();
@@ -50,7 +49,7 @@ public class Game {
         janela.setVisible(true);
 
 
-        this.UpdateState = new Update(player, obstaculo);
+
         this.Renderer = new Render();
 
         System.out.println("Controle de Sprite: \n- 'D' - Correr\n- 'W' - Morte\n Nada - Ocioso");
